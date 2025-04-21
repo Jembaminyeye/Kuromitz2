681e3bcf7f8d554bd283c9193eb02667cccbd795
jembaminyeye <rodriguezpavez.benjamin@gmail.com>
2025-04-21 02:58:34 -0400

    home edit

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
   standalone: false
 })
-export class HomePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HomePage {
+  resenas = [
+    {
+      nombre: 'Ignacia Layana',
+      texto: 'Una obra maestra del cine moderno. La fotografía y la narrativa son excepcionales...'
+    },
+    {
+      nombre: 'ShonFornite',
+      texto: 'La mejor serie que he visto este año. Los personajes están muy bien desarrollados...'
+    },
+    {
+      nombre: 'Matias Ruiz',
+      texto: 'Una propuesta innovadora que rompe con los esquemas tradicionales del kpop...'
+    }
+  ];
 }
