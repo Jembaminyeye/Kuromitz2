62893fac512f086e1c3355cf03ea991c8c49d6a7
Ignacio Layana <ig.layana.silva@gmail.com>
2025-04-22 23:46:57 -0400

    Se arreglan cosas, se añade footer en home, se añade, se cambia nombre a inicio a novedades

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -6,11 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.page.scss'],
   standalone: false
 })
-export class InicioPage implements OnInit {
+export class InicioPage {
+  resenas = [
+    {
+      titulo: 'Juan Pérez',
+      texto: '¡Excelente app para ver películas!',
+      calificacion: 5
+    },
+    {
+      titulo: 'Ana López',
+      texto: 'Muy fácil de usar y con buenas recomendaciones.',
+      calificacion: 4
+    },
+    {
+      titulo: 'Carlos Gómez',
+      texto: 'Me gustaría que tuviera más series.',
+      calificacion: 3
+    }
+  ];
+  
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
 }
+
+
+
