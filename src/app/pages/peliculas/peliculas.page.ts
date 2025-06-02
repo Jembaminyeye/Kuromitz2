35888b9c17352783fd97fd4f4a8a21ef706ea03d
Javier <javier.morales.d@mail.pucv.cl>
2025-06-02 14:57:28 -0400

    Mejoras en páginas de series y películas, imágenes actualizadas

--- src/app/pages/peliculas/peliculas.page.ts
+++ src/app/pages/peliculas/peliculas.page.ts
@@ -11,26 +11,79 @@ export class PeliculasPage {
     {
       titulo: 'MINIONS',
       descripcion: 'Descripción breve de la película con detalles importantes para captar el interés del usuario.',
-      imagen: 'assets/icon/183973.webp',
+      imagen: 'assets/icon/pelis/183973.webp',
       calificacion: 5.0
     },
     {
       titulo: 'Minecraft',
       descripcion: 'Descripción breve de la película con detalles importantes para captar el interés del usuario.',
-      imagen: 'assets/icon/375_375.jpeg',
+      imagen: 'assets/icon/pelis/375_375.jpeg',
       calificacion: 4.1
     },
     {
       titulo: 'Que paso ayer',
       descripcion: 'Descripción breve de la película con detalles importantes para captar el interés del usuario.',
-      imagen: 'assets/icon/images.jfif',
+      imagen: 'assets/icon/pelis/images.jfif',
       calificacion: 4.9
     },
     {
       titulo: 'Batman',
       descripcion: 'Descripción breve de la película con detalles importantes para captar el interés del usuario.',
-      imagen: 'assets/icon/batman.jpg',
+      imagen: 'assets/icon/pelis/batman.jpg',
       calificacion: 4.5
+    },
+  ];
+  populares = [
+    {
+      titulo: 'Dune 2',
+      imagen: 'assets/icon/pelis/dune2.webp',
+      calificacion: 5.0,
+    },
+    {
+      titulo: 'Godzilla x Kong',
+      imagen: 'assets/icon/pelis/god.jpg',
+      calificacion: 4.8,
+    },
+    {
+      titulo: 'The Fall Guy',
+      imagen: 'assets/icon/pelis/fallguy.jpg',
+      calificacion: 4.7,
+    },
+    {
+      titulo: 'Civil War',
+      imagen: 'assets/icon/pelis/civil.jpg',
+      calificacion: 4.6,
+    },
+    {
+      titulo: 'The Beekeeper',
+      imagen: 'assets/icon/pelis/beekeeper.jpg',
+      calificacion: 4.5,
+    },
+  ];
+
+  romanticas = [
+    {
+      titulo: 'A todos los chicos de los que me enamoré',
+      imagen: 'assets/icon/pelis/enamore.webp',
+      calificacion: 4.3,
+    },
+    {
+      titulo: 'El stand de los besos',
+      imagen: 'assets/icon/pelis/stand.jpg',
+      calificacion: 4.0,
+    },
+  ];
+
+  terror = [
+    {
+      titulo: 'El Conjuro',
+      imagen: 'assets/icon/pelis/conjuro.jpg',
+      calificacion: 4.5,
+    },
+    {
+      titulo: 'It',
+      imagen: 'assets/icon/pelis/it.jpg',
+      calificacion: 4.2,
     },
   ];
 }
--- src/app/pages/series/series.page.ts
+++ src/app/pages/series/series.page.ts
@@ -1,16 +1,89 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-series',
   templateUrl: './series.page.html',
   styleUrls: ['./series.page.scss'],
-  standalone: false
+  standalone: false,
 })
-export class SeriesPage implements OnInit {
+export class SeriesPage {
+  series = [
+    {
+      titulo: 'Stranger Things',
+      descripcion: 'Una serie de ciencia ficción y suspenso ambientada en los años 80.',
+      imagen: 'assets/icon/series/stranger things.jpg',
+      calificacion: 4.8
+    },
+    {
+      titulo: 'Breaking Bad',
+      descripcion: 'Un profesor de química se convierte en narcotraficante.',
+      imagen: 'assets/icon/series/breaking.jpg',
+      calificacion: 5.0
+    },
+    {
+      titulo: 'The Office',
+      descripcion: 'Una comedia sobre la vida diaria de trabajadores de oficina.',
+      imagen: 'assets/icon/series/office.webp',
+      calificacion: 4.7
+    },
+    {
+      titulo: 'Game of Thrones',
+      descripcion: 'Una lucha épica por el trono en un mundo fantástico.',
+      imagen: 'assets/icon/series/gameof.jpg',
+      calificacion: 4.6
+    },
+  ];
+  populares = [
+    {
+      titulo: 'Stranger Things',
+      imagen: 'assets/icon/series/stranger things.jpg',
+      calificacion: 5.0,
+    },
+    {
+      titulo: 'The Night Agent',
+      imagen: 'assets/icon/series/agent.webp',
+      calificacion: 4.7,
+    },
+    {
+      titulo: 'Bridgerton',
+      imagen: 'assets/icon/series/bridgerton.webp',
+      calificacion: 4.5,
+    },
+    {
+      titulo: 'The Crown',
+      imagen: 'assets/icon/series/crown.webp',
+      calificacion: 4.6,
+    },
+    {
+      titulo: 'La Casa de Papel',
+      imagen: 'assets/icon/series/papel.jpg',
+      calificacion: 4.8,
+    },
+  ];
 
-  constructor() { }
+  romanticas = [
+    {
+      titulo: 'Emily en París',
+      imagen: 'assets/icon/series/emily.jpg',
+      calificacion: 4.1,
+    },
+    {
+      titulo: 'Outlander',
+      imagen: 'assets/icon/series/outlander.webp',
+      calificacion: 4.3,
+    },
+  ];
 
-  ngOnInit() {
-  }
-
+  terror = [
+    {
+      titulo: 'Marianne',
+      imagen: 'assets/icon/series/mari.webp',
+      calificacion: 4.2,
+    },
+    {
+      titulo: 'The Haunting of Hill House',
+      imagen: 'assets/icon/series/house.jpg',
+      calificacion: 4.6,
+    },
+  ];
 }
